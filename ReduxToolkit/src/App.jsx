af543c0145a94522eaf6b9879923ad77eb965572
StymRwt <satyamrawat010@gmail.com>
2025-05-21 19:12:18 +0530

    first commit

---
+++ ReduxToolkit/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import AddTodo from './store/components/AddTodo'
+import Todos from './store/components/Todos'
+
+const App = () => {
+  return (
+    <>
+    <div>todos</div>
+    <AddTodo/>
+    <Todos/>
+    </>
+  )
+}
+
+export default App
